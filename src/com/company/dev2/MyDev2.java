fbc22223cd4a200f1a92fdf25a35f7c72baf06f4
Pabitra Panda <pk.pabitra18@gmail.com>
2021-07-11 23:41:04 +0530

    removed package name

--- src/com/company/dev2/MyDev2.java
+++ src/com/company/dev2/MyDev2.java
@@ -1,11 +1,7 @@
-package com.company.dev2;
-
 public class MyDev2 {
     public void test(){
 
-        Integer a = 5;
-        Integer b = 7;
-        Integer x = a + b;
-        System.out.println("Result is "+x);
+        Integer x = 5 +6;
+        System.out.println(x);
     }
 }
