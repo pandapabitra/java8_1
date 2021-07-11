bf06176fa709b99707b2ea318cfd2fb3ec386ed8
Pabitra Panda <pk.pabitra18@gmail.com>
2021-07-11 23:52:24 +0530

    removed comment

--- src/com/company/gitoperation/Main.java
+++ src/com/company/gitoperation/Main.java
@@ -1,8 +1,8 @@
 package com.company.gitoperation;
 public class Main {
     public static void main(String[] args) {
+        //write your code here
         Integer z = 55+6;
         System.out.println(z);
-        System.out.println("Main method");
     }
 }
