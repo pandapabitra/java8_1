a30ea2996b43b5b20f780606559e5a9f65fc3c56
Pabitra Panda <pk.pabitra18@gmail.com>
2021-07-11 21:39:49 +0530

    implemented review comments

--- src/com/company/dev2/MyDev2.java
+++ src/com/company/dev2/MyDev2.java
@@ -2,7 +2,6 @@ package com.company.dev2;
 
 public class MyDev2 {
     public void test(){
-        System.out.println("Test method");
         Integer x = 5+6;
         System.out.println(x);
     }
--- src/com/company/gitoperation/Main.java
+++ src/com/company/gitoperation/Main.java
@@ -2,7 +2,7 @@ package com.company.gitoperation;
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        int z = 55+6;;
+        Integer z = 55+6;
         System.out.println(z);
     }
 }
