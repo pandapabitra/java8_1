14525ba3dc4c850f437c5eded3eb0ff9461c7416
Pabitra Panda <pk.pabitra18@gmail.com>
2021-07-11 21:11:01 +0530

    added code for add logic

---
+++ src/com/company/dev2/MyDev2.java
@@ -0,0 +1,9 @@
+package com.company.dev2;
+
+public class MyDev2 {
+    public void test(){
+        System.out.println("Test method");
+        Integer x = 5+6;
+        System.out.println(x);
+    }
+}
